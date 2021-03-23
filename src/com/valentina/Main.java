652de2acea42f70d83463ce84c98e160bbbcbb8c
Valentina <vaanoko@gmail.com>
2021-03-23 22:28:11 +0300

    lesson1

---
+++ src/com/valentina/Main.java
@@ -0,0 +1,13 @@
+package com.valentina;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        String A = reader.nextLine();
+
+        System.out.println("Hello " + A + "!");
+
+    }
+}
